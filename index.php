7c307fb3165c1f2294c8afaaae0180b921914916
Villar-L <62591670+Villar-L@users.noreply.github.com>
2021-02-04 21:56:35 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Welcome to my page Luke Quen B. Villar</p>
+</body>
+</html>
